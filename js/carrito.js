732b73eec7b0c5a9cf103b66e6d1592d6ec4eb91
Ezequiel Leiva Cecchi <ezequielleivacecchi@gmail.com>
2023-08-31 15:50:03 -0300

    ProyectoFinal

--- js/carrito.js
+++ js/carrito.js
@@ -1,111 +1,93 @@
 // Trayendo elementos del DOM y los productos guardados en el localStorage
 let carrito = JSON.parse(localStorage.getItem('carrito')) || [];
-let numeroDeCarrito = JSON.parse(localStorage.getItem("cantidad")) || 0
+let numeroDeCarrito = JSON.parse(localStorage.getItem("cantidad")) || 0;
 let numero = document.getElementById(`numeroDeCarrito`);
 const borrarDeCarrito = document.getElementById("vaciarCarrito");
 const textCart = document.querySelector(".textCart");
 const botonComprar = document.querySelector(`.boton-comprar`);
+const carritoLista = document.getElementById('carrito-lista');
+const precioTotal = document.createElement('div');
+// Función para calcular el subtotal individual
+const obtenerSubtotalIndividual = (producto) => {
+    return producto.precio * producto.cantidad;
+};
 
-// Obtiene el total de todos los productos agregados al carrito
-const obtenerTotal = (carrito) => {
-    let total = 0;
-    for (something of carrito) {
-        total += Number(something.precio)
+// Función para calcular el precio total del carrito
+const obtenerTotal = () => {
+    return Object.values(carrito).reduce((total, producto) => total + obtenerSubtotalIndividual(producto), 0);
+};
+// Función para mostrar los productos en el carrito
+function mostrarProductosEnCarrito() {
+    carritoLista.innerHTML = '';
+
+    if (carrito.length === 0) {
+        textCart.style.display = 'block';
+        precioTotal.style.display = 'none';
+    } else {
+        textCart.style.display = 'none';
+
+        carrito.forEach((producto, index) => {
+            const li = document.createElement('li');
+            // const cantidad = carrito.filter(item => item.id === producto.id).length;
+            li.innerHTML = `
+                <img style="width: 92px" src="${producto.image}">
+                <h6>${producto.titulo}</h6>
+                <p class="product-price">Precio:$${producto.precio} Cantidad:${producto.cantidad} Subtotal:$${obtenerSubtotalIndividual(producto)}</p>
+                <button class="eliminar" onclick="eliminarProducto(${index})"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z"/>
+                <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z"/>
+              </svg></button>`;
+            carritoLista.appendChild(li);
+        });
+
+        precioTotal.innerHTML = `
+            <p>Su precio total es de: $${obtenerTotal(carrito)}</p>`;
+        carritoLista.appendChild(precioTotal);
+        precioTotal.style.display = 'block';
     }
-    return total;
+
+    numero.textContent = carrito.length;
+    localStorage.setItem(`cantidad`, JSON.stringify(carrito.length));
 }
 
-// Funcion encargada de vaciar el carrito y llamarla al apretar un boton
-function vaciarCarrito() {
-    localStorage.removeItem('carrito');
-    carrito = [];
-    mostrarProductosEnCarrito();
+// Función para eliminar un producto del carrito
+function eliminarProducto(index) {
+    if (index >= 0 && index < carrito.length) {
+        carrito.splice(index, 1);
+        localStorage.setItem('carrito', JSON.stringify(carrito));
+        mostrarProductosEnCarrito();
+    }
 }
 
-borrarDeCarrito.addEventListener(`click`,() =>{
+// Evento para vaciar el carrito
+borrarDeCarrito.addEventListener(`click`, () => {
     Swal.fire({
         icon: 'success',
-        title: 'Se vacio el carrito con exito',
-        text: 'Se ha vaciado el carrito con exito'
+        title: 'Se vació el carrito con éxito',
+        text: 'Se ha vaciado el carrito con éxito'
     });
-    if (carrito.length === 0) {
-        Swal.fire({
-            icon: 'error',
-            title: 'No hay ningún producto en el carrito',
-            text: 'No se ha encontrado ningun producto'
-        });
-    }
-     vaciarCarrito();
-}) 
+    vaciarCarrito();
+});
 
-// Evento en el que simula comprar los productos agregados al carrito
+// Evento para simular una compra
 botonComprar.addEventListener("click", () => {
     Swal.fire({
         icon: 'success',
         title: 'Compra Realizada',
         text: 'Se ha realizado la compra exitosamente'
     });
-    if (carrito.length === 0) {
-        Swal.fire({
-            icon: 'error',
-            title: 'No hay ningún producto en el carrito',
-            text: 'Añada un producto que quiera y vuelva a intentar'
-        });
-    }
     vaciarCarrito();
 });
 
-
-// Funcion utilizada para mostrar los productos que se seleccionaron
-function mostrarProductosEnCarrito() {
-    let carritoLista = document.getElementById('carrito-lista');
-    let precioTotal = document.createElement('div');
-
-    carritoLista.innerHTML = '';
-
-    if (carrito.length === 0) {
-        textCart.style.display = 'block';
-    } else {
-        textCart.style.display = 'none';
-    }
-
-    carrito.forEach((producto, index) => {
-        let li = document.createElement('li');
-
-        li.innerHTML = `
-            <img style="width: 92px" src="${producto.image}">
-            <h6>${producto.titulo}</h6>
-            <p class="product-price">$${producto.precio}</p>
-            <button class="eliminar" onclick="eliminarProducto(${index})">Eliminar</button>`;
-        carritoLista.appendChild(li);
-    });
-
-    // Agregar el precio total una vez que todos los productos han sido agregados al carrito
-    precioTotal.innerHTML = `
-<p>Su precio total es de: $${obtenerTotal(carrito)}</p>`;
-    carritoLista.appendChild(precioTotal);
-    if (carrito.length === 0) {
-        precioTotal.style.display = 'none';
-    } else {
-        precioTotal.style.display = 'block';
-    }
+// Función para vaciar el carrito
+function vaciarCarrito() {
+    localStorage.removeItem('carrito');
+    localStorage.removeItem(`cantidad`);
+    carrito = [];
+    mostrarProductosEnCarrito();
 }
 
-
-// Funcion que agrega un boton para eliminar los productos
-function eliminarProducto(index) {
-    if (index >= 0 && index < carrito.length) {
-        carrito.splice(index, 1);
-        console.log(obtenerTotal(carrito))
-        localStorage.setItem('carrito', JSON.stringify(carrito));
-        numero.textContent = carrito.length;
-        localStorage.setItem(`cantidad`, JSON.stringify(carrito.length));
-        mostrarProductosEnCarrito();
-
-    }
-}
-
-// Esta funcion se usa para verificar que la funcion de adentro se ejecute correctamente
+// Cargar los productos en el carrito al cargar la página
 window.onload = function () {
     mostrarProductosEnCarrito();
 };
--- js/products.js
+++ js/products.js
@@ -1,14 +1,15 @@
 let numeritoDeCarrito = document.getElementById(`numeroDeCarrito`);
 let carrito = JSON.parse(localStorage.getItem('carrito')) || [];
 const cartItem = document.getElementById("cartItems");
-numeritoDeCarrito.textContent =  JSON.parse(localStorage.getItem("cantidad")) || 0
+numeritoDeCarrito.textContent = carrito.length || 0;
 
 // Carga datos desde un archivo JSON de manera asíncrona 
 async function loadJSON() {
   const res = await fetch('./productos.json');
   return await res.json();
 }
-// Aca se esta utilizando la funcion asincronica
+
+// Aca se está utilizando la función asincrónica
 loadJSON().then(products => {
   const contenedor = document.getElementById("contentProducts");
   for (let i = 0; i < products.length; i++) {
@@ -24,26 +25,31 @@ loadJSON().then(products => {
     `;
   }
 });
-// Agrega al carrito el producto al que se le indique tocando el boton
+
+// Agrega al carrito el producto al que se le indique tocando el botón
 function agregarAlCarrito(i) {
   loadJSON().then(data => {
-    let producto = data[i]
-    producto.image = "."+producto.image
-    carrito.push(producto);
+    let producto = data[i];
+    producto.image = "." + producto.image;
+    const index = carrito.findIndex(item => item.id === producto.id);
+    if (index !== -1) {
+      carrito[index].cantidad++;
+    } else {
+      // Si no está en el carrito, agregarlo con cantidad 1
+      carrito.push({ ...producto, cantidad: 1 });
+    }
+    // Actualizar los datos en el almacenamiento local
     localStorage.setItem('carrito', JSON.stringify(carrito));
-    // Actualiza el numerito al lado del carrito después de actualizar el carrito
     numeritoDeCarrito.textContent = carrito.length;
-    localStorage.setItem(`cantidad`,JSON.stringify(carrito.length));
-   
-  });
-  Toastify({
+    localStorage.setItem(`cantidad`, carrito.length);
 
-    text: "Se a agregado el producto correctamente",
-   gravity: "bottom",
-    position:"right",
-    avatar:"/assets/Falcon.gaming.png",
-    backgroundColor: "#000000",
-    duration: 1000
-
+    Toastify({
+      text: "Se ha agregado el producto correctamente",
+      gravity: "bottom",
+      position: "right",
+      avatar: "/assets/Falcon.gaming.png",
+      backgroundColor: "#000000",
+      duration: 1000
     }).showToast();
+  });
 }
